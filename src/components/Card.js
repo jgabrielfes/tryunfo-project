6562549fcaf25767302aca815fa33afe76a1af2a
João Gabriel <jgabrielfes@gmail.com>
2022-02-24 23:01:23 -0300

    Cor de fundo das cartas alterado pela raridade :scream:

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,13 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Card.css';
 
+const bgColors = {
+  'normal': '#036b52',
+  'raro': 'firebrick',
+  'muito raro': 'mediumslateblue',
+};
+
 class Card extends React.Component {
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3, cardImage,
       cardRare, cardTrunfo } = this.props;
 
     return (
-      <div className="tryunfo-card">
+      <div className="tryunfo-card" style={ {
+        backgroundColor: bgColors[cardRare],
+      } }>
         <p data-testid="name-card">{ cardName }</p>
         <img src={ cardImage } alt={ cardName } data-testid="image-card" />
         <p data-testid="description-card">{ cardDescription }</p>
